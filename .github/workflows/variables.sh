e157bbbc1590b15e1291f129542cae0203453a78
afsmaira <andre.smaira@alumni.usp.br>
2021-01-24 21:43:41 -0300

    Testing CI

--- .github/workflows/variables.sh
+++ .github/workflows/variables.sh
@@ -13,8 +13,8 @@ echo $PSE_NAME
 echo $SO
 echo $ARCHITECTURE
 
-echo "::set-env name=PSE_VERSION::$PSE_VERSION"
-echo "::set-env name=PSE_URL::$PSE_URL"
-echo "::set-env name=PSE_NAME::$PSE_NAME"
-echo "::set-env name=SO::$SO"
-echo "::set-env name=ARCHITECTURE::$ARCHITECTURE"
+echo "PSE_VERSION=$PSE_VERSION" >> $PSE_VERSION
+echo "PSE_URL=$PSE_URL" >> $PSE_URL
+echo "PSE_NAME=$PSE_NAME" >> $PSE_NAME
+echo "SO=$SO" >> $SO
+echo "ARCHITECTURE=$ARCHITECTURE" >> $ARCHITECTURE
