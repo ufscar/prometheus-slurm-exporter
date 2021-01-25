64641c659fc8d4cae5cfce877d8ba089aae1c154
afsmaira <andre.smaira@alumni.usp.br>
2021-01-24 21:17:30 -0300

    Testing CI

--- .github/workflows/variables.sh
+++ .github/workflows/variables.sh
@@ -12,3 +12,9 @@ echo $PSE_URL
 echo $PSE_NAME
 echo $SO
 echo $ARCHITECTURE
+
+echo "::set-env name=PSE_VERSION::$PSE_VERSION"
+echo "::set-env name=PSE_URL::$PSE_URL"
+echo "::set-env name=PSE_NAME::$PSE_NAME"
+echo "::set-env name=SO::$SO"
+echo "::set-env name=ARCHITECTURE::$ARCHITECTURE"
